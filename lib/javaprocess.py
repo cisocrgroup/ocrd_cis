93723b3424eaa16409582cfb0c61a27bc9a6bd19
flo <flo@cis.lmu.de>
2018-07-19 15:19:50 +0200

    JavaProcess done

--- lib/javaprocess.py
+++ lib/javaprocess.py
@@ -2,10 +2,10 @@ import subprocess
 from ocrd.utils import getLogger
 
 class JavaProcess:
-    def __init__(self, jar, main, input, args):
+    def __init__(self, jar, main, input_str, args):
         self.jar = jar
         self.main = main
-        self.input = input
+        self.input_str = input_str
         self.args = args
         self.log = getLogger('JavaProcess')
 
@@ -17,23 +17,16 @@ class JavaProcess:
                 stdout=subprocess.PIPE,
                 stdin=subprocess.PIPE,
                 encoding='utf-8',
-                #stderr=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
         ) as p:
-            # self.log.info("input: %s", self.get_input())
-            self.output, err = p.communicate(input=self.get_input())
-            # self.log.debug("data: %s", self.output)
-            # for x in str(self.output).split("\n"):
-            #     self.log.debug("x: %s", str(x))
-            self.output = self.output.split("\n")
+            self.output, err = p.communicate(input=self.input_str)
+            self.output = self.output
             retval = p.wait()
             self.log.info("retval: %i", retval)
             if retval != 0:
                 raise ValueError(
                     "cannot execute {}: {}\nreturned: {}"
                     .format(cmd, err, retval))
-
-    def get_input(self):
-        return "\n".join(self.input)
 
     def get_cmd(self):
         cmd = ['java', '-cp', self.jar, self.main]
