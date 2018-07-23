332ddf629b62590f32657604eafcb9853ec4e2a3
flo <flo@cis.lmu.de>
2018-07-23 12:33:11 +0200

    Implement basic test script for alignment

---
+++ tests/align/run_align_test.bash
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -e
+
+PAGE_XML_MIME_TYPE="application/vnd.prima.page+xml"
+CACHE_DIR="/tmp/cis-ocrd-py-cache"
+TMP_DIR=$(mktemp -d -t cis-ocrd-align-XXXXXXXXX)
+
+function rmtd() {
+		echo removing $TMP_DIR
+		rm -rf $TMP_DIR
+}
+trap rmtd EXIT
+
+function wget_cached() {
+		local url=$1
+		local filename=$2
+		local destdir=$TMP_DIR/downloads
+
+		if test ! -f $CACHE_DIR/$filename; then
+				mkdir -p $CACHE_DIR
+				echo "downloading $url/$filename"
+				wget -P $CACHE_DIR $url/$filename
+		fi
+		mkdir -p $destdir
+		ln $CACHE_DIR/$filename $destdir/$filename ||\
+				cp $CACHE_DIR/$filename $destdir/$filename
+}
+
+function download_ocrd_gt_zip() {
+		local url="http://www.ocr-d.de/sites/all/GTDaten"
+		local filename=$1
+		wget_cached $url $filename
+}
+
+function unzip_ocrd_gt() {
+		local zip="$TMP_DIR/downloads/$1"
+		echo unziping $zip
+		unzip -d $TMP_DIR/downloads ${zip/.zip/}
+}
+
+function download_and_unzip_ocrd_gt() {
+		download_ocrd_gt_zip $1
+		unzip_ocrd_gt $1
+}
+
+function get_gt_page_xml_files() {
+		page_xml_files=""
+		for d in $(find $TMP_DIR/downloads -type d -name page); do
+				for f in $(find $d -type f); do
+						page_xml_files+=" $f"
+				done
+		done
+}
+
+function activate_env() {
+		local envdir=$CACHE_DIR/env
+		if test ! -d $envdir; then
+				virtualenv -p python3 $envdir
+				make deps
+				make install
+		fi
+		source $envdir/bin/activate
+}
+
+function download_ocrd_jar() {
+		local url='http://www.cis.lmu.de/~finkf'
+		wget_cached $url "ocrd-0.1.jar"
+}
+
+function setup_test() {
+		download_and_unzip_ocrd_gt $1
+		download_ocrd_jar
+		activate_env
+}
+
+function setup_workspace() {
+		old=$PWD
+		get_gt_page_xml_files
+		id=1
+		echo "{\"cisOcrdJar\":\"$TMP_DIR/downloads/ocrd-0.1.jar\"}" > $TMP_DIR/config.json
+		cd $TMP_DIR
+		ocrd workspace init $TMP_DIR
+		for f in $page_xml_files; do
+				id=$((id+1))
+				sid=$(printf '%x' $id)
+				ocrd workspace add $f -G gt -i gt_$sid -m $PAGE_XML_MIME_TYPE
+
+				id=$((id+1))
+				sid=$(printf '%x' $id)
+				cp $f $f.ocr1
+				ocrd workspace add $f.ocr1 -G ocr1 -i ocr1_$sid -m $PAGE_XML_MIME_TYPE
+
+				id=$((id+1))
+				sid=$(printf '%x' $id)
+				cp $f $f.ocr2
+				ocrd workspace add $f.ocr2 -G ocr2 -i ocr2_$sid -m $PAGE_XML_MIME_TYPE
+		done
+		cd $old
+}
+
+
+setup_test loeber_heuschrecken_1693.zip
+activate_env
+setup_workspace
+
+# align 3 file groups
+cis-ocrd-align --mets $TMP_DIR/mets.xml \
+							 --input-file-grp 'ocr1,ocr2,gt' \
+							 --parameter file://$TMP_DIR/config.json
