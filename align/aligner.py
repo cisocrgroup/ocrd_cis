2d320ae7e86f0e8c9e4128aa7c03ea69413097a5
flo <flo@cis.lmu.de>
2018-07-25 15:51:24 +0200

    Aligner aligns and writes page XML and METS files

--- align/aligner.py
+++ align/aligner.py
@@ -96,7 +96,6 @@ class PageAlignment:
         for region in pcgts.get_Page().get_TextRegion():
             for line in region.get_TextLine():
                 self.log.info("line: %s", line.get_TextEquiv()[0].Unicode)
-                line.get_TextEquiv()[0].set_index(0)
                 current = next(ilist)
                 self.add_line_alignments(line, current)
                 self.add_word_alignments(line, current)
@@ -117,8 +116,8 @@ class PageAlignment:
         """
         k = 0
         for word in page_xml_line.get_Word():
+            word.get_TextEquiv()[0].set_dataType(self.ifgs[0])
             page_xml_word = word.get_TextEquiv()[0].Unicode
-            # skip words that do not contain current (e.g. ' §.')
             if alignment_line.tokens[k][0] in page_xml_word:
                 self.log.debug("word: %s", page_xml_word)
                 for (i, w) in enumerate(alignment_line.tokens[k]):
@@ -135,20 +134,20 @@ class PageAlignment:
         """
         Add alignment TextEquivs to the given page XML line.
         """
-        page_xml_line.get_TextEquiv()[0].set_index(0)
         self.log.debug("line %s", page_xml_line.get_TextEquiv()[0].Unicode)
-        self.log.debug(" - line: %s (%s)", alignment_line.pairwise[0][0], self.ifgs[0])
+        self.log.debug(" - line: %s (%s)",
+                       alignment_line.pairwise[0][0], self.ifgs[0])
+        page_xml_line.get_TextEquiv()[0].set_dataType(self.ifgs[0])
         eq = TextEquivType(
-            index=1,
             dataType="alignment-line-{}".format(self.ifgs[0]),
             Unicode=alignment_line.pairwise[0][0],
         )
         page_xml_line.add_TextEquiv(eq)
-        for i in range(1, len(alignment_line.pairwise)):
-            self.log.debug(" - line: %s (%s)", alignment_line.pairwise[i][1], self.ifgs[i])
+        for i in range(0, len(alignment_line.pairwise)):
+            self.log.debug(" - line: %s (%s)",
+                           alignment_line.pairwise[i][1], self.ifgs[i+1])
             eq = TextEquivType(
-                index=i+1,
-                dataType="alignment-line-{}".format(self.ifgs[i]),
+                dataType="alignment-line-{}".format(self.ifgs[i+1]),
                 Unicode=alignment_line.pairwise[i][1],
             )
             page_xml_line.add_TextEquiv(eq)
