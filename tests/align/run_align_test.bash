d4f211c1f1de0064518f01d1a563e67c6365d52a
flo <flo@cis.lmu.de>
2018-07-31 11:56:52 +0200

    Improve debugging output

--- tests/align/run_align_test.bash
+++ tests/align/run_align_test.bash
@@ -9,19 +9,21 @@ function setup_workspace() {
 		pushd $TMP_DIR
 		ocrd workspace init $TMP_DIR
 		for f in $PAGE_XML_FILES; do
-				echo file $f
 				id=$((id+1))
 				sid=$(printf '%x' $id)
+				echo ocrd workspace add $f -G gt -i gt_$sid -m $PAGE_XML_MIME_TYPE
 				ocrd workspace add $f -G gt -i gt_$sid -m $PAGE_XML_MIME_TYPE
 
 				id=$((id+1))
 				sid=$(printf '%x' $id)
 				sed -e 's/ſ/fl/g' -e 's/ey/ej/g' $f > $f.ocr1
+				echo ocrd workspace add $f -G gt -i gt_$sid -m $PAGE_XML_MIME_TYPE
 				ocrd workspace add $f.ocr1 -G ocr1 -i ocr1_$sid -m $PAGE_XML_MIME_TYPE
 
 				id=$((id+1))
 				sid=$(printf '%x' $id)
 				sed -e 's/ſ/j/g' -e 's/ee/ӓ/g' $f > $f.ocr2
+				echo ocrd workspace add $f -G gt -i gt_$sid -m $PAGE_XML_MIME_TYPE
 				ocrd workspace add $f.ocr2 -G ocr2 -i ocr2_$sid -m $PAGE_XML_MIME_TYPE
 		done
 		popd
