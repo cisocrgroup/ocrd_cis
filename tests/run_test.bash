6749e10bfdfc3224bf5116f9b9ab4777e086acfb
flo <flo@cis.lmu.de>
2018-08-01 11:13:26 +0200

    Implement basic runner script for tests

---
+++ tests/run_test.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TMP_LOG=$(mktemp /tmp/cis-ocrd-run-test-XXXXXX.log)
+function rmtl() {
+		echo removing $TMP_LOG
+		rm -f $TMP_LOG
+}
+trap rmtl EXIT
+
+verbose="false"
+while test $# -gt 0; do
+		case $1 in
+				--verbose|-v)
+						verbose="true"
+						shift
+						;;
+				*)
+						break
+						;;
+		esac
+done
+
+for t in $@; do
+		if test $verbose = "true"; then
+				$t
+				if test $? -ne 0; then
+						exit 1
+				fi
+		else
+				$t &> $TMP_LOG
+				if test $? -ne 0; then
+						exit 1
+				fi
+		fi
+done
