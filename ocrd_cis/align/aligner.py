a9c7e6064a434a519186300bb8aac5225ff91176
flo <flo@cis.lmu.de>
2018-12-07 15:49:24 +0100

    Implement word alignment

--- ocrd_cis/align/aligner.py
+++ ocrd_cis/align/aligner.py
@@ -61,14 +61,14 @@ class Aligner(Processor):
     def align_lines(self, lines):
         """align the given line alignment with the lines"""
         for i, line in enumerate(lines):
-            self.log.info('align line: %s [%s - %s]',
+            self.log.info('line alignment: %s [%s - %s]',
                           line.region.get_TextEquiv()[0].Unicode,
                           line.region.get_id(),
                           line.input_file.input_file_group)
             if i != 0:
                 lines[0].region.add_TextEquiv(line.region.get_TextEquiv()[0])
             lines[0].region.get_TextEquiv()[i].set_comments(
-                line.input_file.input_file_group)
+                line.input_file.input_file_group + "." + line.region.get_id())
         self.align_words(lines)
 
     def align_words(self, lines):
@@ -77,7 +77,7 @@ class Aligner(Processor):
         oregion = [lines[i].region.get_Word() for i in range(1, len(lines))]
 
         for word in lines[0].alignment['words']:
-            self.log.info("aligning word %s", word['master'])
+            self.log.debug("aligning word %s", word['master'])
             master, rest = self.find_word([word['master']], mregion, "master")
             mregion = rest
             if master is None or len(master) != 1:
@@ -89,18 +89,37 @@ class Aligner(Processor):
                     raise Exception("cannot find {}".format(other))
                 others.append(match)
                 oregion[i] = rest
-            self.log.info("word alignment (master): %s",
-                          master[0].get_TextEquiv()[0].Unicode)
-            for other in others:
-                _str = [x.get_TextEquiv()[0].Unicode for x in other]
-                self.log.info("word alignment (other):  %s",
-                              " ".join(_str));
+            words = list()
+            words.append(Alignment(lines[0].input_file, master, lines[0].alignment))
+            for i, other in enumerate(others):
+                words.append(Alignment(lines[i+1].input_file, other, lines[i+1].alignment))
+            self.align_word_regions(words)
+
+    def align_word_regions(self, words):
+        # self.log.info("word alignment (master): %s",
+        #               words[0].region[0].get_TextEquiv()[0].Unicode)
+        # for other in words[1:]:
+        #     _str = [x.get_TextEquiv()[0].Unicode for x in other.region]
+        #     self.log.info("word alignment (other):  %s",
+        #                   " ".join(_str))
+        for i, word in enumerate(words):
+            _str = " ".join([x.get_TextEquiv()[0].Unicode for x in word.region])
+            _id = ",".join([x.get_id() for x in word.region])
+            conf = min([x.get_TextEquiv()[0].get_conf() or 0.0 for x in word.region])
+            self.log.info("word alignment: %s [%s - %s]",
+                          _str,
+                          _id,
+                          word.input_file.input_file_group)
+            if i != 0:
+                words[0].region[0].add_TextEquiv(
+                    TextEquivType(Unicode=_str, conf=conf))
+            words[0].region[0].get_TextEquiv()[i].set_comments(
+                word.input_file.input_file_group + "." + _id)
 
     def find_word(self, tokens, regions, t="other"):
-        self.log.info("tokens = %s [%s]", tokens, t)
+        self.log.debug("tokens = %s [%s]", tokens, t)
         for i, _ in enumerate(regions):
             n = self.match_tokens(tokens, regions, i)
-            self.log.info("n = %d", n)
             if n == 0:
                 continue
             return tuple([regions[i:n], regions[i:]])
@@ -110,7 +129,6 @@ class Aligner(Processor):
             tokens, t)
         for i, _ in enumerate(regions):
             n = self.match_tokens_lev(tokens, regions, i)
-            self.log.info("n = %d", n)
             if n == 0:
                 continue
             return tuple([regions[i:n], regions[i:]])
@@ -136,8 +154,8 @@ class Aligner(Processor):
         for j, token in enumerate(tokens):
             if j + i > len(regions):
                 return 0
-            self.log.info('checking %s with %s', token,
-                          regions[i+j].get_TextEquiv()[0].Unicode)
+            self.log.debug('checking %s with %s', token,
+                           regions[i+j].get_TextEquiv()[0].Unicode)
             if f(token, regions[i+j].get_TextEquiv()[0].Unicode):
                 continue
             if j == 0:
@@ -147,7 +165,6 @@ class Aligner(Processor):
             # match ath the first token position
             i += 1
         return i + len(tokens)
-
 
     def open_input_file_tuples(self, ift):
         """
@@ -175,31 +192,6 @@ class Aligner(Processor):
             files.append(ifiles)
         return zip(*files)
 
-    def write_alignment_to_xml(self, pa):
-        """
-        Write the alignments into new output-file-group.
-        The alignment is done by the master file (first index).
-        """
-        self.log.info("writing alignment to %s", self.output_file_grp)
-        master = pa.ifs[0]  # master input file group
-        pcgts = from_file(self.workspace.download_file(master))
-        ilist = iter(pa.line_alignments)
-        for region in pcgts.get_Page().get_TextRegion():
-            for line in region.get_TextLine():
-                line.get_TextEquiv()[0].Unicode.strip()
-                self.log.debug("line: %s", line.get_TextEquiv()[0].Unicode)
-                current = next(ilist)
-                pa.add_line_alignments(line, current)
-                pa.add_word_alignments(line, current)
-        self.log.debug("master basename: %s", master.basename)
-        self.add_output_file(
-            ID="{}_{}".format(master.ID, self.output_file_grp),
-            mimetype=MIMETYPE_PAGE,
-            content=to_xml(pcgts),
-            file_grp=self.output_file_grp,
-            basename=master.basename,
-        )
-
     def read_lines_from_input_file(self, ifile):
         self.log.info("reading input file: %s", ifile)
         lines = list()
@@ -221,72 +213,6 @@ class Aligner(Processor):
         p = JavaAligner(self.parameter['cisOcrdJar'], n)
         return p.run("\n".join(_input))
 
-
-class PageAlignment:
-    """PageAlignment holds a list of LineAlignments."""
-
-    def __init__(self, ifgs, ifs, process_output):
-        """
-        Create a page alignment from java-aligner's output.
-
-        It reads blocks of len(ifs) lines. Each block is assumed to be a
-        valid input for the LineAlignment constructor.
-        """
-        self.ifgs = ifgs
-        self.ifs = ifs
-        self.log = getLogger('cis.PageAlignment')
-        self.line_alignments = list()
-        lines = process_output.split("\n")
-        n = len(self.ifs)
-        for i in range(0, len(lines), n):
-            self.line_alignments.append(LineAlignment(lines[i:i+n]))
-
-    def add_word_alignments(self, page_xml_line, alignment_line):
-        """
-        Add word alignments to the words of the given page XML line.
-        We iterate over the master-OCR words, so the first word of the
-        tuple must be contained in the given page XML word.
-        """
-        k = 0
-        for word in page_xml_line.get_Word():
-            word.get_TextEquiv()[0].set_dataType(self.ifgs[0])
-            page_xml_word = word.get_TextEquiv()[0].Unicode
-            if (k < len(alignment_line.tokens) and
-                    alignment_line.tokens[k][0] in page_xml_word):
-                self.log.debug("word: %s", page_xml_word)
-                for (i, w) in enumerate(alignment_line.tokens[k]):
-                    self.log.debug(" - word: %s (%s)", w, self.ifgs[i])
-                    eq = TextEquivType(
-                        index=i+1,
-                        dataType='alignment-token-{}'.format(self.ifgs[i]),
-                        Unicode=w,
-                    )
-                    word.add_TextEquiv(eq)
-                k += 1
-
-    def add_line_alignments(self, page_xml_line, alignment_line):
-        """
-        Add alignment TextEquivs to the given page XML line.
-        """
-        self.log.debug("line %s", page_xml_line.get_TextEquiv()[0].Unicode)
-        self.log.debug(" - line: %s (%s)",
-                       alignment_line.pairwise[0][0], self.ifgs[0])
-        page_xml_line.get_TextEquiv()[0].set_dataType(self.ifgs[0])
-        eq = TextEquivType(
-            dataType="alignment-line-{}".format(self.ifgs[0]),
-            Unicode=alignment_line.pairwise[0][0],
-        )
-        page_xml_line.add_TextEquiv(eq)
-        for i in range(0, len(alignment_line.pairwise)):
-            self.log.debug(" - line: %s (%s)",
-                           alignment_line.pairwise[i][1], self.ifgs[i+1])
-            eq = TextEquivType(
-                dataType="alignment-line-{}".format(self.ifgs[i+1]),
-                Unicode=alignment_line.pairwise[i][1],
-            )
-            page_xml_line.add_TextEquiv(eq)
-
-
 class FileAlignment:
     def __init__(self, ifile, ifg):
         self.input_file = ifile
@@ -303,54 +229,3 @@ class Alignment:
         self.input_file = ifile
         self.region = region
         self.alignment = alignment
-
-
-hash_escape = "\u0E23"
-dollar_escape = "\u0E24"
-underscore_escape = "\u0E5F"
-
-
-def escape_for_aligner(text):
-    """escapes # with 0xE023 and $ with 0xE024"""
-    text = text.replace("#", hash_escape)
-    text = text.replace("$", dollar_escape)
-    text = text.replace("_", underscore_escape)
-    return text
-
-
-def unescape_from_aligner(text):
-    """escapes 0xE023 with # and 0xE024 with $"""
-    text = text.replace(hash_escape, "#")
-    text = text.replace(dollar_escape, "$")
-    return text
-
-
-class LineAlignment:
-    """
-    LineAlignment holds a line alignment.
-    A line alignment of n lines holds n-1 pairwise alignments
-    and a list of token alignments of n-tuples.
-
-    Each pairwise alignment represents the alignment of the
-    master line with another. Pairwise aligned lines have always
-    the same length. Underscores ('_') mark deletions or insertions.
-    """
-
-    def __init__(self, lines):
-        """
-        Create a LineAlignment from n-1 pairwise
-        alignments an one token alignment at pos n-1.
-        """
-        self.n = len(lines)
-        self.pairwise = list()
-        for i in range(0, self.n-1):
-            self.pairwise.append(tuple(lines[i].split(",")))
-        self.tokens = list()
-        for ts in lines[self.n-1].split(","):
-            self.tokens.append(tuple(ts.split(":")))
-
-    def __str__(self):
-        data = {}
-        data['pairwise'] = self.pairwise
-        data['tokens'] = self.tokens
-        return json.dumps(data)
