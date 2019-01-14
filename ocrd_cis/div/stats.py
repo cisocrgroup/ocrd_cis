dbbb667c423a729c06babba1d1c195a02711f4e7
flo <flo@cis.lmu.de>
2019-01-14 15:08:45 +0100

    Use index as dict key (easier) and print dict as json

--- ocrd_cis/div/stats.py
+++ ocrd_cis/div/stats.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
+import json
 from ocrd.utils import getLogger, concat_padded, xywh_from_points, points_from_x0y0x1y1
 from ocrd import Processor, MIMETYPE_PAGE
 from ocrd_cis import get_ocrd_tool
 from ocrd.model.ocrd_page_generateds import parse, parsexml_, parsexmlstring_
 from Levenshtein import distance
-
 
 
 class Stats(Processor):
@@ -17,6 +17,16 @@ class Stats(Processor):
         super(Stats, self).__init__(*args, **kwargs)
         self.log = getLogger('Stats')
 
+    def get_gt_index(self, regions):
+        for region in regions:
+            for line in region.get_TextLine():
+                i = 0
+                for te in line.get_TextEquiv():
+                    ddt = te.get_dataTypeDetails()
+                    if ddt.startswith('OCR-D-GT'):
+                        return i
+                    i += 1
+        raise ValueError("cannot determine gt-index")
 
     def process(self):
         """
@@ -24,14 +34,14 @@ class Stats(Processor):
         """
 
         d = dict()
-        if "gt" not in d:
-            d["gt"]=0
+        # if "gt" not in d:
+        #     d["gt"] = 0
 
         inputfiles = self.input_files
         for input_file in inputfiles:
 
-            #index = input_file.url.rfind('/')
-            #alignurl = input_file.url[:index] + '/' + self.input_file_grp + input_file.url[index:]
+            # index = input_file.url.rfind('/')
+            # alignurl = input_file.url[:index] + '/' + self.input_file_grp + input_file.url[index:]
             alignurl = input_file.url
 
             pcgts = parse(alignurl, True)
@@ -39,38 +49,39 @@ class Stats(Processor):
             page = pcgts.get_Page()
             regions = page.get_TextRegion()
 
+            # find index of GT
+            gti = self.get_gt_index(regions)
+            if gti not in d:
+                d[gti] = 0
 
             for region in regions:
                 lines = region.get_TextLine()
 
                 for line in lines:
 
-                    gtline = line.get_TextEquiv()[1].Unicode
-                    d["gt"] += len(gtline)
+                    gtline = line.get_TextEquiv()[gti].Unicode
+                    d[gti] += len(gtline)
 
-                    Type = line.get_TextEquiv()[0].dataType[9:]
+                    #Type = line.get_TextEquiv()[0].dataType[9:]
 
+                    # for i in range(2, len(line.get_TextEquiv())):
+                    for i in range(0, gti):
+                        # print("%d: %s".format(i, line.get_
+                        # OCRType = line.get_TextEquiv()[i].dataType
+                        # lindex = OCRType.find('OCR-D-')
+                        # rindex = OCRType.find(Type)
+                        # model = OCRType[lindex:rindex-1]
 
-                    for i in range(2,len(line.get_TextEquiv())):
-                        OCRType = line.get_TextEquiv()[i].dataType
-                        lindex = OCRType.find('OCR-D-')
-                        rindex = OCRType.find(Type)
-                        model = OCRType[lindex:rindex-1]
-
-                        
-                        if model not in d:
-                            d[model]=0
-
-                        #print(line.get_TextEquiv()[2].dataType)
+                        if i not in d:
+                            d[i] = 0
+                        # print(line.get_TextEquiv()[2].dataType)
                         unicodeline = line.get_TextEquiv()[i].Unicode
 
-                        d[model] += distance(gtline, unicodeline)
-
-
+                        d[i] += distance(gtline, unicodeline)
 
                         # words = line.get_Word()
                         # for word in words:
                         #     for ocr in word.get_TextEquiv():
                         #         print(ocr.Unicode)
 
-        print(d)
+        print(json.dumps(d))
