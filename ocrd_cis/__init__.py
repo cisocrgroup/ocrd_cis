502e3373de6a3e9e760381542dcbc78513f99c39
flo <flo@cis.lmu.de>
2019-01-21 15:27:52 +0100

    Export Training and Evaluation java classes

--- ocrd_cis/__init__.py
+++ ocrd_cis/__init__.py
@@ -2,4 +2,6 @@ from .javaprocess import JavaProcess
 from .javaprocess import JavaTrain
 from .javaprocess import JavaAligner
 from .javaprocess import JavaProfiler
+from .javaprocess import JavaEvalDLE
+from .javaprocess import JavaEvalRRDM
 from .ocrd_tool import get_ocrd_tool
