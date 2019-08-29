ae34f9275c5ae2b95450bf47a5aa6987267ab7e2
flo <flo@cis.lmu.de>
2019-08-29 10:25:32 +0200

    capture log level string for java subprocesses

--- ocrd_cis/profile/cli.py
+++ ocrd_cis/profile/cli.py
@@ -7,9 +7,14 @@ from ocrd_utils import getLogger
 from ocrd_cis import get_ocrd_tool
 from ocrd_cis import JavaProfiler
 
+LOG_LEVEL = 'INFO'
+
 @click.command()
 @ocrd_cli_options
 def cis_ocrd_profile(*args, **kwargs):
+    global LOG_LEVEL
+    if 'log_level' in kwargs:
+        LOG_LEVEL = kwargs['log_level']
     return ocrd_cli_wrap_processor(Profiler, *args, **kwargs)
 
 class Profiler(Processor):
@@ -21,9 +26,11 @@ class Profiler(Processor):
         self.log = getLogger('cis.Processor.Profiler')
 
     def process(self):
+        global LOG_LEVEL
         self.log.debug("starting java client")
+        self.log.debug("LOG_LEVEL = %s", LOG_LEVEL)
         p = JavaProfiler(self.workspace.mets_target, self.input_file_grp,
-                         self.output_file_grp, self.parameter, "DEBUG")
+                         self.output_file_grp, self.parameter, LOG_LEVEL)
         p.exe()
         # Reload the updated METS file to make sure that run_processor
         # does not overwrite the updated file with the old.
