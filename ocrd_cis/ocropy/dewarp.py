3bbb3ff0981f47b83a68a5b195f927a35d52128c
Robert Schubert <robert.schubert@mathint.com>
2019-09-24 12:59:51 +0200

    allow specifying extra output file group for images:
    - clip/denoise/deskew/dewarp/resegment: try to split given
      output file grp into PAGE and image result
    - use fixed OCR-D-IMG-* only as fallback
    - parse the parameter member only when in appropriate contexts
      (because Processor.__init__ does not initialize this when
       -J or --help is requested)

--- ocrd_cis/ocropy/dewarp.py
+++ ocrd_cis/ocropy/dewarp.py
@@ -19,7 +19,7 @@ from .common import (
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 LOG = getLogger('processor.OcropyDewarp')
-FILEGRP_IMG = 'OCR-D-IMG-DEWARP'
+FALLBACK_FILEGRP_IMG = 'OCR-D-IMG-DEWARP'
 
 # from ocropus-dewarp, but without resizing
 def dewarp(image, lnorm, check=True):
@@ -49,10 +49,16 @@ class OcropyDewarp(Processor):
         kwargs['ocrd_tool'] = self.ocrd_tool['tools']['ocrd-cis-ocropy-dewarp']
         kwargs['version'] = self.ocrd_tool['version']
         super(OcropyDewarp, self).__init__(*args, **kwargs)
-
-        # defaults from ocrolib.lineest:
-        range_ = self.parameter['range']
-        self.lnorm = lineest.CenterNormalizer(params=(range_, 1.0, 0.3))
+        if hasattr(self, 'output_file_grp'):
+            # defaults from ocrolib.lineest:
+            range_ = self.parameter['range']
+            self.lnorm = lineest.CenterNormalizer(params=(range_, 1.0, 0.3))
+            try:
+                self.page_grp, self.image_grp = self.output_file_grp.split(',')
+            except ValueError:
+                self.page_grp = self.output_file_grp
+                self.image_grp = FALLBACK_FILEGRP_IMG
+                LOG.info("No output file group for images specified, falling back to '%s'", FALLBACK_FILEGRP_IMG)
 
     def process(self):
         """Dewarp the lines of the workspace.
@@ -65,8 +71,9 @@ class OcropyDewarp(Processor):
         into the higher-level image), and dewarp it (without resizing).
         Export the result as an image file.
         
-        Add the new image file to the workspace with a fileGrp USE equal
-        ``OCR-D-IMG-DEWARP`` and an ID based on input file and input element.
+        Add the new image file to the workspace with a fileGrp USE given
+        in the second position of the output fileGrp, or ``OCR-D-IMG-DEWARP``,
+        and an ID based on input file and input element.
         
         Reference each new image in the AlternativeImage of the element.
         
@@ -75,9 +82,9 @@ class OcropyDewarp(Processor):
         
         for (n, input_file) in enumerate(self.input_files):
             LOG.info("INPUT FILE %i / %s", n, input_file.pageId or input_file.ID)
-            file_id = input_file.ID.replace(self.input_file_grp, FILEGRP_IMG)
+            file_id = input_file.ID.replace(self.input_file_grp, self.image_grp)
             if file_id == input_file.ID:
-                file_id = concat_padded(FILEGRP_IMG, n)
+                file_id = concat_padded(self.image_grp, n)
             
             pcgts = page_from_file(self.workspace.download_file(input_file))
             page_id = pcgts.pcGtsId or input_file.pageId or input_file.ID # (PageType has no id)
@@ -119,7 +126,7 @@ class OcropyDewarp(Processor):
                         dew_image,
                         file_id + '_' + region.id + '_' + line.id,
                         page_id=input_file.pageId,
-                        file_grp=FILEGRP_IMG)
+                        file_grp=self.image_grp)
                     # update PAGE (reference the image file):
                     alternative_image = line.get_AlternativeImage()
                     line.add_AlternativeImage(AlternativeImageType(
@@ -127,19 +134,17 @@ class OcropyDewarp(Processor):
                         comments=line_xywh['features'] + ',dewarped'))
             
             # update METS (add the PAGE file):
-            file_id = input_file.ID.replace(self.input_file_grp,
-                                            self.output_file_grp)
+            file_id = input_file.ID.replace(self.input_file_grp, self.page_grp)
             if file_id == input_file.ID:
-                file_id = concat_padded(self.output_file_grp, n)
-            file_path = os.path.join(self.output_file_grp,
-                                     file_id + '.xml')
+                file_id = concat_padded(self.page_grp, n)
+            file_path = os.path.join(self.page_grp, file_id + '.xml')
             out = self.workspace.add_file(
                 ID=file_id,
-                file_grp=self.output_file_grp,
+                file_grp=self.page_grp,
                 pageId=input_file.pageId,
                 local_filename=file_path,
                 mimetype=MIMETYPE_PAGE,
                 content=to_xml(pcgts))
             LOG.info('created file ID: %s, file_grp: %s, path: %s',
-                     file_id, self.output_file_grp, out.local_filename)
+                     file_id, self.page_grp, out.local_filename)
     
