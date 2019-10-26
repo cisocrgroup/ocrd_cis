69716d9ee2f2c54549d6a802ae4c66d8ffe95c8a
Konstantin Baierer <unixprog@gmail.com>
2019-10-26 21:15:40 +0200

    javaprocess.py: Capture STDERR properly

--- ocrd_cis/javaprocess.py
+++ ocrd_cis/javaprocess.py
@@ -104,12 +104,12 @@ class JavaProcess:
 
         cmd = self.get_cmd()
         self.log.info('command: %s', " ".join(cmd))
-        ret = subprocess.run(cmd)
+        ret = subprocess.run(cmd, stderr=subprocess.PIPE, check=False, universal_newlines=True)
         self.log.debug("%s: %i", " ".join(cmd), ret.returncode)
         if ret.returncode != 0:
             raise ValueError(
                 "cannot execute {}: {}\n{}"
-                .format(" ".join(cmd), ret.returncode, err.decode('utf-8')))
+                .format(" ".join(cmd), ret.returncode, ret.stderr))
 
     def log_stderr(self, err):
         for line in err.decode("utf-8").split("\n"):
