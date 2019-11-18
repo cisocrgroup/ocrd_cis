4e21f2f34e26d90b0945522f22c2ae5d87ca4967
flo <flo@cis.lmu.de>
2019-11-18 17:19:24 +0100

    Improve output

--- data/misc/ocrd-cis-dita.sh
+++ data/misc/ocrd-cis-dita.sh
@@ -21,13 +21,13 @@ EOF
 
 	# tool
 	cat <<EOF > "$dir/tool.md"
-# Tool $tool
+# Tool $tool {#Tool .concept}
 $(cat ocrd-tool.json | jq -r ".tools.\"$tool\".description")
 EOF
 
 	# parameters
 	cat <<EOF > "$dir/parameters.md"
-# Parameters
+# Parameters {#parameters .reference}
 The tool $tool accepts the following configuration parameters:
 \`\`\`json
 $(cat ocrd-tool.json | jq ".tools.\"$tool\".parameters")
@@ -36,7 +36,7 @@ EOF
 
 	# installation
 	cat <<EOF > "$dir/installation.md"
-# Installation of $tool
+# Installation of $tool {#installation .task}
 1. (optional) Initialize virtualenv: \`python3 -m venv path/to/dir\`
 2. Install ocrd_cis: \`make install\`
 EOF
@@ -47,18 +47,18 @@ EOF
 		if echo "$line" | grep $tool > /dev/null; then
 			# echo "setting blockn=1"
 			ofile="$dir/Description.md"
-			echo "# Description of $tool" > "$ofile"
+			echo "# Description of $tool {#Description .concept}" > "$ofile"
 			blockn=1
 		elif [[ $blockn == 1 ]] && [[ "$line" == "" ]]; then
 			# echo "setting blockn=2"
 			ofile="$dir/Option.md"
-			echo "# Options for $tool" > "$ofile"
+			echo "# Options for $tool {#Option .reference}" > "$ofile"
 			blockn=2
 		elif [[ $blockn == 2 ]] && [[ "$line" == "" ]]; then
 			# echo "setting blockn=0"
 			blockn=0
 		elif [[ $blockn == 1 ]] || [[ $blockn == 2 ]]; then
-			echo "$blockn $line";
+			# echo "$blockn $line";
 			echo "$line" >> "$ofile"
 		fi
 	done < README.md
