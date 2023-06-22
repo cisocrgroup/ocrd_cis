43a356a03221c6dd95b68c9bc0cb7b563f2b4870
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-06-22 02:48:09 +0200

    postcorrect: improve/update OCR-D wrapper…
    - discern constructor vs. processing ctxt (initLogging)
    - pass effective log level (instead of global CLI override)
    - use mets_target instead of fixed `mets.xml`
    - simplify

--- ocrd_cis/postcorrect/cli.py
+++ ocrd_cis/postcorrect/cli.py
@@ -5,19 +5,14 @@ import os
 from ocrd import Processor
 from ocrd.decorators import ocrd_cli_options
 from ocrd.decorators import ocrd_cli_wrap_processor
-from ocrd_utils import getLogger
+from ocrd_utils import getLogger, getLevelName
 from ocrd_models.ocrd_mets import OcrdMets
 from ocrd_cis import JavaPostCorrector
 from ocrd_cis import get_ocrd_tool
 
-LOG_LEVEL = 'INFO'
-
 @click.command()
 @ocrd_cli_options
 def ocrd_cis_postcorrect(*args, **kwargs):
-    if 'log_level' in kwargs and kwargs['log_level']:
-        global LOG_LEVEL
-        LOG_LEVEL = kwargs['log_level']
     return ocrd_cli_wrap_processor(PostCorrector, *args, **kwargs)
 
 class PostCorrector(Processor):
@@ -26,21 +21,22 @@ class PostCorrector(Processor):
         kwargs['ocrd_tool'] = ocrd_tool['tools']['ocrd-cis-postcorrect']
         kwargs['version'] = ocrd_tool['version']
         super(PostCorrector, self).__init__(*args, **kwargs)
-        self.log = getLogger('cis.Processor.PostCorrector')
 
     def process(self):
-        ifgs = self.input_file_grp.split(",")  # input file groups
-        ofg = self.output_file_grp
+        self.log = getLogger('processor.CISPostCorrector')
         profiler = {}
         profiler["path"] = self.parameter["profilerPath"]
         profiler["config"] = self.parameter["profilerConfig"]
         profiler["noCache"] = True
         self.parameter["profiler"] = profiler
         self.parameter["runDM"] = True
-        metspath = os.path.join(self.workspace.directory, "mets.xml")
-        print(json.dumps(self.parameter, indent=4))
-        p = JavaPostCorrector(metspath, ",".join(ifgs), ofg, self.parameter, LOG_LEVEL)
+        self.log.debug(json.dumps(self.parameter, indent=4))
+        p = JavaPostCorrector(self.workspace.mets_target,
+                              self.input_file_grp,
+                              self.output_file_grp,
+                              self.parameter,
+                              getLevelName(self.log.getEffectiveLevel()))
         p.exe()
         # reload the mets file to prevent it from overriding the
         # updated version from the java process
-        self.workspace.mets = OcrdMets(filename=metspath)
+        self.reload_mets()
