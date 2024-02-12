320d5fd69d2b6efbacaee9b70275cf5e66c9794e
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2024-02-12 15:44:19 +0100

    try to fix tests by adapting URLs

--- tests/test_lib.bash
+++ tests/test_lib.bash
@@ -4,11 +4,13 @@ tmpdir=$(mktemp -d)
 trap "rm -rf $tmpdir" EXIT
 
 OCRD_CIS_FILEGRP="OCR-D-GT-SEG-LINE"
-data_url="https://ocr-d-repo.scc.kit.edu/api/v1/dataresources/75ad9f94-dbaa-43e0-ab06-2ce24c497c61/data"
+# fixme: it does not work like this - the OCR-D GT repo uses different URL paths for different datasets
+# this is merely the path for blumenbach_anatomie_1805.ocrd.zip
+data_url="https://ocr-d-repo.scc.kit.edu/api/v1/dataresources/75ad9f94-dbaa-43e0-ab06-2ce24c497c61/data/"
 function ocrd_cis_download_bagit() {
 	local url="$data_url/$1"
 	mkdir -p "$tmpdir/download"
-	wget -P "$tmpdir/download" "$url"
+	wget -nc -P "$tmpdir/download" "$url"
 }
 
 function ocrd_cis_init_ws() {
@@ -19,33 +21,16 @@ function ocrd_cis_init_ws() {
 
 function ocrd_cis_align() {
 	# download ocr models
-	wget -P "$tmpdir/download" "http://cis.lmu.de/~finkf/fraktur1-00085000.pyrnn.gz"
-	wget -P "$tmpdir/download" "http://cis.lmu.de/~finkf/fraktur2-00062000.pyrnn.gz"
+	ocrd resmgr download ocrd-cis-ocropy-recognize fraktur.pyrnn.gz
+	ocrd resmgr download ocrd-cis-ocropy-recognize fraktur-jze.pyrnn.gz
 	# run ocr
-	ocrd-cis-ocropy-recognize --log-level DEBUG \
-							  --input-file-grp "OCR-D-GT-SEG-LINE" \
-							  --output-file-grp OCR-D-CIS-OCR-1 \
-							  --mets "$tmpws/mets.xml" \
-							  --parameter <(cat <<EOF
-{
-	"textequiv_level": "word",
-	"model": "$tmpdir/download/fraktur1-00085000.pyrnn.gz"
-}
-EOF
-										   )
-	ocrd-cis-ocropy-recognize --log-level DEBUG \
-							  --input-file-grp "OCR-D-GT-SEG-LINE" \
-							  --output-file-grp OCR-D-CIS-OCR-2 \
-							  --mets "$tmpws/mets.xml" \
-							  --parameter <(cat <<EOF
-{
-	"textequiv_level": "word",
-	"model": "$tmpdir/download/fraktur2-00062000.pyrnn.gz"
-}
-EOF
-										   )
-	ocrd-cis-align --log-level DEBUG \
-				   -I OCR-D-CIS-OCR-1,OCR-D-CIS-OCR-2,OCR-D-GT-SEG-LINE \
-				   -O OCR-D-CIS-ALIGN \
-				   -m $tmpws/mets.xml
+	ocrd-cis-ocropy-recognize -l DEBUG -m $tmpws/mets.xml \
+ 				-I $OCRD_CIS_FILEGRP -O OCR-D-CIS-OCR-1 \
+				-P textequiv_level word -P model fraktur.pyrnn.gz
+	ocrd-cis-ocropy-recognize -l DEBUG -m $tmpws/mets.xml \
+				-I $OCRD_CIS_FILEGRP -O OCR-D-CIS-OCR-2 \
+				-P textequiv_level word -P model fraktur-jze.pyrnn.gz
+	ocrd-cis-align -l DEBUG -m $tmpws/mets.xml \
+				-I OCR-D-CIS-OCR-1,OCR-D-CIS-OCR-2,$OCRD_CIS_FILEGRP \
+				-O OCR-D-CIS-ALIGN 
 }
