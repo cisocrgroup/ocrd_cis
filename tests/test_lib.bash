c4f0724f97dc5880a64835d40ed265a540c4b2fd
joschrew <jonas.schrewe@gwdg.de>
2024-02-13 14:27:05 +0100

    Another try to fix tests

--- tests/test_lib.bash
+++ tests/test_lib.bash
@@ -4,13 +4,12 @@ tmpdir=$(mktemp -d)
 trap "rm -rf $tmpdir" EXIT
 
 OCRD_CIS_FILEGRP="OCR-D-GT-SEG-LINE"
-# fixme: it does not work like this - the OCR-D GT repo uses different URL paths for different datasets
-# this is merely the path for blumenbach_anatomie_1805.ocrd.zip
-data_url="https://ocr-d-repo.scc.kit.edu/api/v1/dataresources/75ad9f94-dbaa-43e0-ab06-2ce24c497c61/data/"
+data_url="http://hdl.handle.net/21.11156/6B119B3C-A24A-424C-AC3C-27E64B051780"
 function ocrd_cis_download_bagit() {
-	local url="$data_url/$1"
-	mkdir -p "$tmpdir/download"
-	wget -nc -P "$tmpdir/download" "$url"
+	local destdir="$tmpdir/download"
+	mkdir -p "$destdir"
+	local dest="$destdir/$1"
+	wget -nc -O $dest $data_url
 }
 
 function ocrd_cis_init_ws() {
@@ -32,5 +31,5 @@ function ocrd_cis_align() {
 				-P textequiv_level word -P model fraktur-jze.pyrnn.gz
 	ocrd-cis-align -l DEBUG -m $tmpws/mets.xml \
 				-I OCR-D-CIS-OCR-1,OCR-D-CIS-OCR-2,$OCRD_CIS_FILEGRP \
-				-O OCR-D-CIS-ALIGN 
+				-O OCR-D-CIS-ALIGN
 }
